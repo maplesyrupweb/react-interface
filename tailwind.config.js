c8c1dc0d6dc5d6b91721aed20d63d9d4579b8d51
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-09-18 20:30:35 -0700

    AddAppointment and AppointmentInfo components

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -9,3 +12,5 @@ module.exports = {
   },
   plugins: [ require('@tailwindcss/forms')]
 }
+
+
