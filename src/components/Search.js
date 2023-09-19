33c3d0c26cd13b124160e01f5646198b5c686881
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-09-18 23:37:24 -0700

    Add toggle for sort button

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,42 @@
-import { BiSearch } from "react-icons/bi";
-import { BiCaretDown } from "react-icons/bi";
+import { BiSearch, BiCaretDown, BiCheck } from "react-icons/bi";
+import { useState } from "react";
 
+const DropDown = ( {toggle} ) => {
+  if (!toggle) {
+    console.log("not true " + toggle);
+    return null;
+  }
+  else {
+    console.log("true " + toggle);
+    return (
+      <div className="origin-top-right absolute right-0 mt-2 w-56
+    rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+    <div className="py-1" role="menu" aria-orientation="vertical" aria-labelledby="options-menu">
+      <div
+        className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+        role="menuitem">Pet Name <BiCheck /></div>
+      <div
+        className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+        role="menuitem">Owner Name  <BiCheck /></div>
+      <div
+        className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+        role="menuitem">Date <BiCheck /></div>
+      <div
+        className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer border-gray-1 border-t-2"
+        role="menuitem">Asc <BiCheck /></div>
+      <div
+        className="px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 flex justify-between cursor-pointer"
+        role="menuitem">Desc <BiCheck /></div>
+    </div>
+  </div>
+  )
+  }
+  
+  
+}
 
 const Search = () => {
+  let [toggleSort, setToggleSort] = useState(false);
     return (
         <div className="py-5">
         <div className="mt-1 relative rounded-md shadow-sm">
@@ -15,9 +49,11 @@ const Search = () => {
           <div className="absolute inset-y-0 right-0 flex items-center">
             <div>
               <button type="button"
+                onClick={() => { setToggleSort(!toggleSort)}}
                 className="justify-center px-4 py-2 bg-blue-400 border-2 border-blue-400 text-sm text-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 flex items-center" id="options-menu" aria-haspopup="true" aria-expanded="true">
                 Sort By <BiCaretDown className="ml-2" />
               </button>
+              <DropDown toggle={toggleSort}/>
             </div>
           </div>
         </div>
