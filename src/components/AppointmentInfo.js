48e3b43f51c61d945387c55ea82025174e953e2c
maplesyrupweb <73809301+maplesyrupweb@users.noreply.github.com>
2023-09-25 20:49:32 -0700

    Add commments for AppointmentInfo.js

--- src/components/AppointmentInfo.js
+++ src/components/AppointmentInfo.js
@@ -1,15 +1,21 @@
 import { BiTrash } from "react-icons/bi"
 import { BiEdit } from "react-icons/bi"
 
+// appointment, onDeleteAppointment, onEditAppointment are in App.js
+
 const AppointmentInfo = ({appointment, onDeleteAppointment, onEditAppointment }) => {
 
     return(
         <li className="px-3 py-3 flex items-start">
-        <button onClick = { ()=> onDeleteAppointment(appointment.id)} type="button"
+        <button onClick = { ()=> 
+        // delete button
+        onDeleteAppointment(appointment.id)} type="button"
         className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
         <BiTrash /></button>
 
-        <button onClick = { ()=> onEditAppointment(appointment.id)} type="button"
+        <button onClick = { ()=> 
+        // edit button
+        onEditAppointment(appointment.id)} type="button"
         className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
         <BiEdit /></button>
 
